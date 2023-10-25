bf0d0d2512ed6ee44dc8cee7b8dccd8f32fff789
kingvi <2198626335@qq.com>
2023-10-25 20:20:22 +0800

    最终版本压缩空间

---
+++ src/views/content/message/QuoteProductList.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "QuoteProductList"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/foot/MyFooter.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "MyFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
